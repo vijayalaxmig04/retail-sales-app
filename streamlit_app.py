4be34a942b655974ff62afe01bdf0b4e18ddf15f
vijayalaxmig04 <vijayalaximig04@gmail.com>
2026-04-02 21:48:14 +0530

    Refactor streamlit_app.py for improved functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 import pandas as pd
-import matplotlib.pyplot as 
+import matplotlib.pyplot as plt
 
 # ---------------- LOGIN SYSTEM ----------------
 def login():
